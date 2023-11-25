727fe5d23854835826450a25a153cde7dabc3c55
soriful15 <sorifulislam.rs10@gmailcomm>
2023-11-25 18:22:36 +0600

    get all user done

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -26,6 +26,28 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+// get all users
+const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const result = await UserServices.getAllUserFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'User not found',
+      error: {
+        code: 404,
+        description: 'User not found!',
+      },
+    });
+  }
+};
+
 export const UserControllers = {
   createUser,
+  getAllUser,
 };
--- src/modules/users/users.route.ts
+++ src/modules/users/users.route.ts
@@ -3,4 +3,5 @@ import { UserControllers } from './users.controller';
 const router = express.Router();
 
 router.post('/users', UserControllers.createUser);
+router.post('/users', UserControllers.getAllUser);
 export const usersRoutes = router;
